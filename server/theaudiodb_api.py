01529e6a7b721672b7db43344d6cb60f3bebee7f
Maxence Lagalle <contact@maxence-lagalle.fr>
2022-04-19 10:46:17 +0200

    Requête get_artist_id_from_name

--- server/theaudiodb_api.py
+++ server/theaudiodb_api.py
@@ -1,3 +1,15 @@
 import requests
 
 
+def get_artist_id_from_name(artist_name: str) -> int:
+    payload = {"s": artist_name}
+    r = requests.get(
+        "https://www.theaudiodb.com/api/v1/json/2/search.php", params=payload)
+    if r.status_code == 200:
+        r = r.json()
+        if r['artists'] is not None:
+            return r.json()['artists'][0]['idArtist']
+        else:
+            raise KeyError
+    else:
+        raise ConnectionError
