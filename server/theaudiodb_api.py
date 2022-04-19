e45cff46ffc72dac5b96802878ab3284fac7458d
Maxence Lagalle <contact@maxence-lagalle.fr>
2022-04-19 12:03:07 +0200

    Récupérer Nom de la chanson et Clip

--- server/theaudiodb_api.py
+++ server/theaudiodb_api.py
@@ -17,7 +17,7 @@ def get_artist_id_from_name(artist_name: str) -> int:
     if r.status_code == 200:
         r = r.json()
         if r['artists'] is not None:
-            return r['artists'][0]['idArtist']
+            return int(r['artists'][0]['idArtist'])
         else:
             raise KeyError
     else:
@@ -32,12 +32,12 @@ def get_random_album_id_from_artist_id(artist_id: int) -> int:
         r = r.json()
         if r['album'] is not None:
             random_album_index = randrange(0, len(r['album']))
-            return r['album'][random_album_index]['idAlbum']
+            return int(r['album'][random_album_index]['idAlbum'])
     else:
         raise ConnectionError
 
 
-def get_random_track_name_from_album_id(album_id: int) -> str:
+def get_random_track_id_from_album_id(album_id: int) -> int:
     payload = {"m": album_id}
     r = requests.get(
         "https://theaudiodb.com/api/v1/json/2/track.php", params=payload)
@@ -45,6 +45,17 @@ def get_random_track_name_from_album_id(album_id: int) -> str:
         r = r.json()
         if r['track'] is not None:
             random_track_index = randrange(0, len(r['track']))
-            return r['track'][random_track_index]['strTrack']
+            return int(r['track'][random_track_index]['idTrack'])
+    else:
+        raise ConnectionError
+
+
+def get_random_clip_from_artist_id(artist_id: int) -> dict:
+    payload = {"i": artist_id}
+    r = requests.get("https://theaudiodb.com/api/v1/json/2/mvid.php", params=payload)
+    if r.status_code == 200:
+        r = r.json()
+        random_clip_index = randrange(0, len(r['mvids']))
+        return {"title": r['mvids'][random_clip_index]['strTrack'], "suggested_youtube_url": r['mvids'][random_clip_index]['strMusicVid']}
     else:
         raise ConnectionError
